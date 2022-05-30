04902b8149451fdba28c5993b2cdc9d22626c29b
Abidan Triguero <abidantriguero@gmail.com>
2022-05-30 12:09:02 -0400

    Variables en JavaScript

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Abidan";
+
+//declarar
+var edad;
+
+edad
+// inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
